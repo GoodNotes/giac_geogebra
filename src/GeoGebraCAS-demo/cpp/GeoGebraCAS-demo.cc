52a4c6ee450d59572be60834d3454ece7f49935e
Novak Novakovic <novak.n@goodnotesapp.com>
2025-02-25 00:51:37 +0100

    Working build, updated giac.

--- src/GeoGebraCAS-demo/cpp/GeoGebraCAS-demo.cc
+++ src/GeoGebraCAS-demo/cpp/GeoGebraCAS-demo.cc
@@ -1,26 +1,33 @@
 #include "GeoGebraCAS.h"
 #include <iostream>
-
+#include <unistd.h>
 #ifdef _WIN32
 #include "tchar.h"
-int _tmain(int argc, _TCHAR* argv[]) {
+#include <unistd.h>
+int _tmain(int argc, _TCHAR *argv[])
+{
 #else
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 #endif
+        //char ch;
+        //cin >> ch;
+        //sleep(20);
         cout << "Starting GeoGebraCAS-demo" << endl;
-        // initializeCAS();
-        string ret = evaluateCAS("factor(x^2-1)");
-        cout << ret << endl;
-        ret = evaluateCAS("evalf(7,13)");
-        cout << ret << endl;
-        ret = evaluateCAS("evalf(7,15)");
-        cout << ret << endl;
-        ret = evaluateCAS("expand((a+b)^3)");
-        cout << ret << endl;
-        ret = evaluateCAS("evalfa(when ( type(((x)^(2))+(1)) == DOM_SYMBOLIC && type(x) == DOM_SYMBOLIC , (assume(x),solve(((x)^(2))+(1),x))[size(assume(x),solve(((x)^(2))+(1),x))-1] , when ( type(((x)^(2))+(1)) == DOM_IDENT && type(x) == DOM_SYMBOLIC && ((x)^(2))+(1) == 'x', (assume(x),solve(((x)^(2))+(1)=0,x))[size(assume(x),solve(((x)^(2))+(1)=0,x))-1] ,when ( size(x) == 1,flatten1((normal([op(solve(((x)^(2))+(1),x))]))),(normal([op(solve(((x)^(2))+(1),x))])) ) ) ))");
-        cout << ret << endl;
-        ret = evaluateCAS("mean([1,2,3,4,1,2,3])");
-        cout << ret << endl;
+        string ret;
+        for (int ii = 0; ii < 100; ii++)
+        {
+                initializeCAS();
+                ret = evaluateCAS("evalf(sum(1/(n!*1), n, 0,800))");
+                cout << ii << "\t" << ret << endl;
+                //evaluateCAS("rm_all_vars()");
+                //clearContextCAS();
+        }
+
+        clearGlobalVars();
+
         cout << "Finishing GeoGebraCAS-demo" << endl;
+        //char c;
+        //cin >> c;
         return 0;
 }
--- src/giac/cpp/misc.cc
+++ src/giac/cpp/misc.cc
@@ -7732,10 +7732,22 @@ static define_unary_function_eval (__os_version,&_os_version,_os_version_s);
 #endif
 
   // step by step utilities
-  gen rm_nontrig(const gen &g){
-    if (g.type!=_SYMB || g._SYMBptr->sommet==at_exp || g._SYMBptr->sommet==at_sin || g._SYMBptr->sommet==at_cos || g._SYMBptr->sommet==at_tan)
+  gen rm_nontrig(const gen &g,const gen &x){
+    if (g.type!=_SYMB)
       return g;
-    return g._SYMBptr->feuille;
+    gen f=g._SYMBptr->feuille;
+    if (g._SYMBptr->sommet==at_exp || g._SYMBptr->sommet==at_sin || g._SYMBptr->sommet==at_cos || g._SYMBptr->sommet==at_tan){
+      vecteur vx=lvarx(f,x);
+      for (int i=0;i<vx.size();++i){
+        if (vx[i].type!=_SYMB)
+          return g;
+        if (vx[i]._SYMBptr->sommet==at_exp || vx[i]._SYMBptr->sommet==at_sin || vx[i]._SYMBptr->sommet==at_cos || vx[i]._SYMBptr->sommet==at_tan)
+          continue;
+        return g;
+      }
+      return f;
+    }
+    return f;
   }
 
   bool is_periodic(const gen & f,const gen & x,gen & periode,GIAC_CONTEXT){
@@ -7745,7 +7757,7 @@ static define_unary_function_eval (__os_version,&_os_version,_os_version_s);
       vecteur w(vx);
       // remove non trig rootnodes up to fixpoint
       for (unsigned i=0;i<vx.size();++i)
-        vx[i]=rm_nontrig(vx[i]);
+        vx[i]=rm_nontrig(vx[i],x);
       vx=lvarx(vx,x);
       if (vx==w)
         break;
@@ -7786,6 +7798,22 @@ static define_unary_function_eval (__os_version,&_os_version,_os_version_s);
     periode=ratnormal(cst_two_pi/periode);
     return !is_zero(periode);
   }
+
+  gen _periode(const gen & a,GIAC_CONTEXT){
+    if (a.type==_STRNG && a.subtype==-1) return  a;
+    vecteur v=gen2vecteur(a);
+    if (v.size()==1)
+      v.push_back(ggb_var(a));
+    if (v.size()!=2)
+      return gensizeerr(contextptr);
+    gen g=v[0],x=v[1],T;
+    if (!is_periodic(g,x,T,contextptr))
+      return undef;
+    return T;
+  }
+  static const char _periode_s []="periode";
+  static define_unary_function_eval (__periode,&_periode,_periode_s);
+  define_unary_function_ptr5( at_periode ,alias_at_periode,&__periode,0,true);
 
   bool in_domain(const gen & df,const gen &x,const gen & x0,GIAC_CONTEXT){
     if (df==x)
